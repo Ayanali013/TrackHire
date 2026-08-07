2eddd1e321c106cee1f020520b9428c703c5c58c
Ayanali013 <ayanalii@student.iul.ac.in>
2026-08-07 22:12:36 +0530

    Db Store Data

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,16 +1,32 @@
 import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
 
 const registerUser = async (req, res) => {
-console.log("1")
+
     const {name , email ,password } =  req.body
-    console.log("2")
     
+  
+   
     const existUser = await User.findOne({email}) 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword)
+
+    const user = await User.create({
+    name : name ,
+    email : email,
+    password: hashedPassword
+});
+
     console.log(existUser)
+    if (existUser){
+        return res.json({
+            message:"User already exist"
+        })
+    }
     
 
-    res.status(200).json({
+    return res.status(200).json({
         message: "Data Received"
     });
 
