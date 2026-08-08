927a3fecc85afacee4058fc480dc369128603c9b
Ayanali013 <ayanalii@student.iul.ac.in>
2026-08-08 22:43:31 +0530

    Login Updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,28 +4,19 @@ import jwt from "jsonwebtoken";
 
 
 const registerUser = async (req, res) => {
-
     const {name , email ,password } =  req.body
-    
-  
-   
     const existUser = await User.findOne({email}) 
-    
     if (existUser){
         return res.json({
             message:"User already exist"
         })
     }
-    
     const hashedPassword = await bcrypt.hash(password, 10);
-
-
     const user = await User.create({
     name : name ,
     email : email,
     password: hashedPassword
 });
-
 const token = jwt.sign(
     {
         id: user._id
@@ -35,19 +26,34 @@ const token = jwt.sign(
         expiresIn: "7d"
     }
 );
-
-
-
-   
-    
-
     return res.status(200).json({
         message: "Data Received",
-        token
-        
-        
+        token 
     });
-
 };
-
 export { registerUser };
+
+const loginUser = async (req,res)=>{
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        console.log(user)
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        // Next step: compare password
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export {loginUser};
