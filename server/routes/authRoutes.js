7d4b2f82b5c777bb060a973d840a0e1235cca6cd
Ayanali013 <ayanalii@student.iul.ac.in>
2026-08-09 22:15:36 +0530

    Authentication Completed

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { registerUser ,loginUser} from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser)
+router.get("/profile", authMiddleware, (req, res) => {
+
+    
+    res.json({
+        message: "Profile route reached"
+    });
+});
 
 export default router;
