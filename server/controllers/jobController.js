0bbf0dd734aab145f8a96b720f725edfd8c20ba5
Ayanali013 <ayanalii@student.iul.ac.in>
2026-08-11 23:01:48 +0530

    Job Backend Completed

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,7 +1,30 @@
-const createJob = async (req, res) => {
-    try {
+import Job from "../models/jobs.js";
 
-        // Job creation logic will come here
+const createJob = async (req , res) => {
+    try {
+        const {title , company, description , location , salary , skills } =  req.body
+
+        const existingJob = await Job.findOne({title ,company, location }) 
+        
+        if (existingJob){
+            return  res.json({
+            message:"Already applied for job in this Company"
+        })
+        }
+
+        const job = await Job.create({
+            title : title,
+            company : company,
+            description : description,
+             location:  location,
+               salary:   salary,
+               skills:skills
+        })
+        return res.json({
+            message:"Job Saved Succesfully."
+        })
+
+        
 
     } catch (error) {
         console.error(error);
