83466e6c61cbb8bcc103d4e7cfeacd0ec6dcdc91
Ayanali013 <ayanalii@student.iul.ac.in>
2026-09-09 00:52:21 +0530

    Basic Candidate Frontend Done

---
+++ clients/app/candidate/(authorized)/applications/page.jsx
@@ -0,0 +1,12 @@
+import PageLayout from "../page";
+
+export default function MyApplications() {
+
+  return (
+    <div>
+        <PageLayout/>
+      <h2>My Applications</h2>
+      {/* Applications content */}
+    </div>
+  );
+}
---
+++ clients/app/candidate/(authorized)/profile/page.jsx
@@ -0,0 +1,11 @@
+import PageLayout from "../page";
+
+export default function Profile() {
+  return (
+    <div>
+      <PageLayout/>
+      <h2>Profile</h2>
+      {/* Profile content */}
+    </div>
+  );
+}
